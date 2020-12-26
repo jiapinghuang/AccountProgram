25c5a3cde2711cfb1242576abffa234918133631
jiapinghuang <1053517232@qq.com>
2020-12-26 11:26:52 +0800

    简单登陆、存储

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -15,17 +15,47 @@ App({
         traceUser: true,
       })
     }
-     // 展示本地存储能力
-      var logs = wx.getStorageSync('logs') || []
-      logs.unshift(Date.now())
-      wx.setStorageSync('logs', logs)
 
-
-  
-    function CheckSession(params) {
-
+    
+    try {
+      var value = wx.getStorageSync('openid')
+      if (value) {
+          console.log("本地缓存："+value)
+      }else{
+          console.log("重新请求数据：")
+          this.getSession();
+      }
+    } catch (e) {
+        console.log(e);
     }
-    function getSession() {    
+  },
+  globalData: {
+    userInfo: null
+  },
+  //获取授权
+  info:function(){
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })   
+  },
+  //登陆和获得openid过程
+  getSession:function() {    
         // 登录
         wx.login({
           success: res => {
@@ -39,38 +69,31 @@ App({
                         '&secret='+secret+'&js_code='+res.code+               
                         '&grant_type=authorization_code',           
                 success:(res)=>{
-                    console.log(res)                  
+                    console.log(res.data.session_key);
+                    console.log(res.data.openid);
+                      //本地存储
+                      wx.setStorageSync('openid', res.data.openid),
+                      wx.setStorageSync('session_key',res.data.session_key)                  
                 }           
               })
             } else{
               console.log('获取用户登录态失败！' + res.errMsg)
             }
           }
-        })
-      // 获取用户信息
-      wx.getSetting({
-        success: res => {
-          if (res.authSetting['scope.userInfo']) {
-            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-            wx.getUserInfo({
-              success: res => {
-                // 可以将 res 发送给后台解码出 unionId
-                this.globalData.userInfo = res.userInfo
-
-                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                // 所以此处加入 callback 以防止这种情况
-                if (this.userInfoReadyCallback) {
-                  this.userInfoReadyCallback(res)
-                }
-              }
-            })
-          }
-        }
-      })   
-    }
+        }) 
   },
-  
-  globalData: {
-    userInfo: null
+  checksession:function(){
+    wx.checkSession({
+      success () {
+        //session_key 未过期，并且在本生命周期一直有效
+        console.log(res,'登录未过期')
+      },
+      fail () {
+        // session_key 已经失效，需要重新执行登录流程
+        this.getSession();
+      }
+    })
+
   }
+
 })
