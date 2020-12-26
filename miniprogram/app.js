7e5544f9e03645ee028fc1318ca048456d686d70
jiapinghuang <1053517232@qq.com>
2020-12-26 16:06:18 +0800

    授权获得信息

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -15,8 +15,9 @@ App({
         traceUser: true,
       })
     }
-
-    
+  
+    //登陆流程：获取授权——得到用户信息——后台进行login得到openid——本地缓存openid——验证登陆是否超时——重新拉取login。
+   // console.log(this.globalData.userInfo)
     try {
       var value = wx.getStorageSync('openid')
       if (value) {
@@ -27,32 +28,31 @@ App({
       }
     } catch (e) {
         console.log(e);
-    }
+    }  
   },
   globalData: {
     userInfo: null
   },
-  //获取授权
-  info:function(){
-      // 获取用户信息
+  onLoad: function() {
+    this.getUserInfo();
+  },
+  //获取授权、用户信息
+  getUserInfo:function(){
+    console.log("进入getUserInfo")
+      // 查看是否授权
       wx.getSetting({
-        success: res => {
+        success (res){
           if (res.authSetting['scope.userInfo']) {
-            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
             wx.getUserInfo({
-              success: res => {
-                // 可以将 res 发送给后台解码出 unionId
-                this.globalData.userInfo = res.userInfo
-                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                // 所以此处加入 callback 以防止这种情况
-                if (this.userInfoReadyCallback) {
-                  this.userInfoReadyCallback(res)
-                }
+              success: function(res) {
+                console.log(res.userInfo)
               }
             })
           }
         }
-      })   
+      })
+    console.log("结束getUserInfo") 
   },
   //登陆和获得openid过程
   getSession:function() {    
--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -5,18 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo')//使用授权模块
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+     // 查看是否授权
+     wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              //console.log(res.userInfo)         
+            }
+          })
+        }
+      }
+    })
   },
-
- 
-
+  //授权成功回调存储数据
+  bindGetUserInfo (e) {
+    this.setData({
+      city:e.detail.userInfo.city,
+      nickName:e.detail.userInfo.nickName,
+      province:e.detail.userInfo.province
+    });
+   //console.log("bindGetUserInfo方法的："+e.detail.userInfo)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
