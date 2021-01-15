1aeb17a5d5d4764524580238f6d0682329e5e6ca
jiapinghuang <1053517232@qq.com>
2021-01-15 15:16:05 +0800

    首页逻辑90%

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -12,7 +12,13 @@ Page({
     defaultDate:dateUtil.currentDate,
     minDate:dateUtil.getMinDate(year,month),
     maxDate:dateUtil.getMaxDate(year,month),
-    selectDate:dateUtil.selectDate
+    selectDate:dateUtil.selectDate,
+    OMmoney:0,//某月统计
+    IMmoney:0,
+    ODmoney:0,//某天统计
+    IDmoney:0,
+    IaccountDetail:[],//账本信息数组
+    OaccountDetail:[]
   },
   //日期开始
   onDisplay() {
@@ -41,36 +47,126 @@ Page({
       },
       complete: res => {
         console.log('callFunction test result: ', res)
+        this.showDayDetail(res.result.data)
       }
     })
   },
+  //展示账本信息
+  showDayDetail:function(obj) {
+    this.calulateMMoney(obj,"d")
+    console.log('-------')
+    let Iarr=[]
+    let Oarr=[]
+    obj.forEach(item=>{
+      if(item.item_type==="O"){
+        Iarr.push(item)
+      }else{
+        Oarr.push(item)
+      }    
+    })
+    this.setData({
+      IaccountDetail:Iarr,
+      OaccountDetail:Oarr
+    })
+  }
+  ,
   callSelectCurrentMon:function(){
     var minDate=dateUtil.formatDate(this.data.minDate)
     var maxDate=dateUtil.formatDate(this.data.maxDate)
     console.log(minDate)
-    // wx.cloud.callFunction({
-    //   // 云函数名称
-    //   name: 'selectCurrentMon',
-    //   // 传给云函数的参数
-    //   data: {
-    //     //一个月的范围
-    //     minDate:parseInt(minDate),
-    //     maxDate:parseInt(maxDate) 
-    //   },
-    //   complete: res => {
-    //     console.log('callFunction test result: ', res)
-    //   }
-    // })
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'selectCurrentMon',
+      // 传给云函数的参数
+      data: {
+        //一个月的范围
+        minDate:parseInt(minDate),
+        maxDate:parseInt(maxDate) 
+      },
+      complete: res => {
+        console.log('callFunction test result: ', res.result.data)      
+        this.calulateMMoney(res.result.data,"m")
+      }
+    })
+  },
+
+  //计算金额收支
+  calulateMMoney:function(obj,type) {
+    //数组按IO分类
+    var Omoney=0
+    var Imoney=0
+    obj.forEach(item=>{
+      if(item.item_type==="O"){
+        Omoney-=parseInt(item.money)
+      }else{
+        Imoney+=parseInt(item.money)
+      }    
+    })
+    if(type==="m"){
+      this.setData({
+        OMmoney:Omoney,
+        IMmoney:Imoney
+      })
+    }else{
+      this.setData({
+        ODmoney:Omoney,
+        IDmoney:Imoney
+      })
+    }
+   
   },
   test:function() {
-   // this.callSelectCurrentMon()
+    var obj=[
+      {
+        addDate: 20210114,
+        del: false,
+        desc: "面试",
+        item_name: "交通",
+        item_type: "O",
+        money: "1000",
+        openid: "ok3nA4orVFYQ2ttc9b_YSOw0i9lQ",
+        _id: "b45a21d55fffecaa04c610cd214ce471"
+      },
+      {
+        addDate: 20210111,
+        del: false,
+        desc: "55",
+        item_name: "教育",
+        item_type: "O",
+        money: "55",
+        openid: "ok3nA4orVFYQ2ttc9b_YSOw0i9lQ",
+        _id: "023ce9555ffffd20046cfad149ac45ff",
+      },
+      {
+        addDate: 20210103,
+        del: false,
+        desc: "跑滴滴",
+        item_name: "交通",
+        item_type: "I",
+        money: "330",
+        openid: "ok3nA4orVFYQ2ttc9b_YSOw0i9lQ",
+        _id: "21ded5cb6001054c0508cb5456e30de9"
+      },
+      {
+        addDate: 20210106,
+        del: false,
+        desc: "租车服务",
+        item_name: "教育",
+        item_type: "I",
+        money: "100",
+        openid: "ok3nA4orVFYQ2ttc9b_YSOw0i9lQ",
+        _id: "023ce9556001056f048025172d40789d"
+      }
+    ]
+    this.showDayDetail(obj)
   },
    /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //this.callSelectAccount()
-   console.log(dateUtil.formatDate(new Date(2010, 0, 1).getTime())) 
+    this.callSelectCurrentMon()
+    this.callSelectAccount()
+   
   },
     /**
    * 生命周期函数--监听页面显示
